717ac3ba9745f0cc453407161001641ba7e2d38b
kingorianderson <kingorianderson@gmail.com>
2022-01-29 18:19:40 -0800

    Updates

---
+++ static/js/main.e113b115.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpnew=this.webpackJsonpnew||[]).push([[0],{9:function(e,n,t){"use strict";t.r(n);var c=t(1),r=t.n(c),o=t(3),s=t.n(o),i=t(0),d=function(){return Object(i.jsx)("div",{children:"Hello world!"})};s.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(d,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.e113b115.chunk.js.map

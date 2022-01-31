33bf7c0fec91afdfb8c9c6d2c6dabfbe2b5b3042
kingorianderson <kingorianderson@gmail.com>
2022-01-31 11:10:48 -0800

    work in progress jan 31 commit 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Announcement from '../components/Announcement';
+import Categories from '../components/Categories';
 import Navbar from '../components/Navbar';
+import Products from '../components/Products';
 import Slider from '../components/Slider';
 
 const Home = () => {
-  return <div>
+  return( 
+  <div>
       <Announcement/>
       <Navbar/>
       <Slider/>
-  </div>;
+      <Categories/>
+      <Products/>
+  </div>
+  )
 };
 
 export default Home;

f6f36e33054817ca037ad585874fd30aaf74ed97
kingorianderson <kingorianderson@gmail.com>
2022-02-03 13:25:10 -0800

    commit feb 3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,11 @@
-import { Facebook, Instagram, Twitter } from "@material-ui/icons";
+import { Facebook, Instagram, MailOutline, Phone, Room, Twitter } from "@material-ui/icons";
 import styled from "styled-components";
 
 
-const Container = styled.div`
-  display: flex;
-`;
-const Left = styled.div`
-  flex: 1;
-`;
-
-const Logo = styled.h1``;
-const Desc = styled.p``;
-const SocialContainer = styled.div``;
-const SocialIcon = styled.div``;
-
-const Center = styled.div`
-  flex: 1;
-`;
-const Right = styled.div`
-  flex: 1;
-`;
 
 
 const Footer = () => {
-  return 
+  return (
   <Container>
      <Left>
          <Logo>Kings.</Logo>
@@ -31,20 +13,44 @@ const Footer = () => {
                It is conducted over computers, tablets, smartphones, and other smart devices.
          </Desc>
          <SocialContainer>
-             <SocialIcon>
+             <SocialIcon color="0d0d0d">
                  <Twitter/>
              </SocialIcon>
-             <SocialIcon>
+
+             <SocialIcon color="0d0d0d">
                  <Facebook/>
              </SocialIcon>
-             <SocialIcon>
+
+             <SocialIcon color="0d0d0d">
                  <Instagram/>
              </SocialIcon>
          </SocialContainer>
      </Left>
-     <Center></Center>
-     <Right></Right>
-  </Container>;
+     <Center>
+
+         <Title>Useful Links</Title>
+         <List>
+             <ListItem>Home</ListItem>
+             <ListItem>Cart</ListItem>
+             <ListItem>Man Fashion</ListItem>
+             <ListItem>Woman Fashion</ListItem>
+             <ListItem>Accessories</ListItem>
+             <ListItem>My Account</ListItem>
+             <ListItem>My Account</ListItem>
+             <ListItem>Order Tracking</ListItem>
+             <ListItem>Wishlist</ListItem>
+             <ListItem>Terms</ListItem>
+         </List>
+     </Center>
+     <Right>
+         <Title>Contact</Title>
+         <ContactItem><Room style={{marginRight:"10px"}}/>Nairobi, Kenya</ContactItem>
+         <ContactItem><Phone style={{marginRight:"10px"}}/>+254 712 333 631</ContactItem>
+         <ContactItem><MailOutline style={{marginRight:"10px"}}/>contact@kingori.co.ke</ContactItem>
+         <Payment src="https://i.ibb.co/Njjh9rh/payment.png"/>
+     </Right>
+  </Container>
+  );
 };
 
 export default Footer;

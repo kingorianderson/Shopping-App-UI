98efc1019ed850b88ad2906b98a0263ecf5605fb
kingorianderson <kingorianderson@gmail.com>
2022-02-04 15:37:28 -0800

    commit feb 4

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+
+
+
+
+const ProductList = () => {
+  return (
+    <Container>
+        <Navbar/>
+        <Announcement/>
+        <Title>Dresses</Title>
+        <FilterContainer>
+            <Filter>
+                <FilterText>Filter Products:</FilterText>
+                <Select>
+                    <Option disabled selected>
+                        Color
+                    </Option>
+                    <Option>White</Option>
+                    <Option>Black</Option>
+                    <Option>Yellow</Option>
+                    <Option>Green</Option>
+                    <Option>Red</Option>
+                </Select>
+
+                <Select>
+                    <Option disabled selected>
+                        Size
+                    </Option>
+                    <Option>XS</Option>
+                    <Option>S</Option>
+                    <Option>M</Option>
+                    <Option>L</Option>
+                    <Option>XL</Option>
+                </Select>
+            </Filter>
+            <Filter>
+            <FilterText>Sort Products:</FilterText>
+            <Select>
+                <Option selected>Newest</Option>
+                <Option>Price (asc)</Option>
+                <Option>Price (desc)</Option>
+            </Select>
+            </Filter>
+        </FilterContainer>
+        <Products/>
+        <Newsletter/>
+        <Footer/>
+    </Container>
+  )
+};
+
+export default ProductList;

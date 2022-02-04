c3d551c584619db8f9e876876447f07a16f33849
kingorianderson <kingorianderson@gmail.com>
2022-02-04 15:39:26 -0800

    commit feb 4

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -6,7 +6,28 @@ import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
 
 
-
+const Container = styled.div``;
+const Title = styled.div`
+  margin: 20px;
+`;
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const Filter = styled.div`
+  margin: 20px;
+`;
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+`;
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
+const Option = styled.option``;
+                                                                                            
 
 const ProductList = () => {
   return (

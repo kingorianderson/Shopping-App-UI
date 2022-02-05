b9af6dcfd80131fb4e287dfbc469178f829029ac
kingorianderson <kingorianderson@gmail.com>
2022-02-05 11:03:35 -0800

    commit feb 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <Product/>;
+  return <Register/>;
 };
 
 export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled
+
+const Login = () => {
+  return (
+      <Container>
+
+      </Container>
+  )
+};
+
+export default Login;

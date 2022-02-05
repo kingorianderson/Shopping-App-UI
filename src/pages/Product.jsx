4c35347552519237e4d2a6ff0a0d02f3c50a4dd3
kingorianderson <kingorianderson@gmail.com>
2022-02-05 08:52:53 -0800

    commit feb 5

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@material-ui/core";
+
 import { Add, Remove } from "@material-ui/icons";
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
@@ -40,6 +40,7 @@ const Price = styled.span`
 const FilterContainer = styled.div`
   width: 50%;
   margin: 30px 0px;
+  display: flex;
   justify-content: space-between;
 `;    
 const Filter = styled.div`
@@ -64,6 +65,38 @@ const FilterSize = styled.select`
 `;  
 const FilterSizeOption = styled.option``;  
 
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 5px;
+`;
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+  &:hover{
+      background-color: #f8f4f4;
+  }
+`;
 
 const Product = () => {
   return (

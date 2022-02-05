d80920c8288f1efb0f285e4121acea808d5e28d6
kingorianderson <kingorianderson@gmail.com>
2022-02-05 11:48:15 -0800

    commit feb 5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,16 @@ const Container = styled
 const Login = () => {
   return (
       <Container>
-
+         <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder="username" />
+          <Input placeholder="password" />
+          <Button>LOGIN</Button>
+          <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
+          <Link>CREATE A NEW ACCOUNT</Link>
+        </Form>
+      </Wrapper>
       </Container>
   )
 };

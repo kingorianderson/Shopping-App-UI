339dff4d51456b9ca67e3a4bea6a569f0c32e805
kingorianderson <kingorianderson@gmail.com>
2022-02-04 20:45:20 -0800

    commit feb 4

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,14 +4,43 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Newsletter from "../components/Newsletter";
 
-const Container = styled.div``;
-const Wrapper = styled.div``;
-const ImgContainer = styled.div``;
-const Image = styled.img``;
-const InfoContainer = styled.div``;
-const Title = styled.h1``;
-const Desc = styled.p``;
-const Price = styled.span``;
+const Container = styled.div`
+  
+`;
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+  
+`;
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+const Image = styled.img`
+ width: 50%;
+ height: 100vh;
+ object-fit: cover;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+const Title = styled.h1`
+  font-weight: 200;
+`;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div``;    
+const Filter = styled.div``;  
+const FilterTitle = styled.span``;  
+const FilterColor = styled.div``;  
+const FilterSize = styled.select``;  
+const FilterSizeOption = styled.option``;  
 
 
 const Product = () => {
@@ -27,6 +56,24 @@ const Product = () => {
                   <Title>Tracksuit</Title>
                   <Desc>Tracksuit with half zip sweatshirt and skinny sweatpants.</Desc>
             <Price>Kes 1500</Price>
+            <FilterContainer>
+                <Filter>
+                    <FilterTitle>Color</FilterTitle>
+                    <FilterColor color="armygreen" />
+                    <FilterColor color="grey" />
+                </Filter>
+
+                <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+            </FilterContainer>
               </InfoContainer>
           </Wrapper>
           <Newsletter/>

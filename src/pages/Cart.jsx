310cf90605dff134cc69ae5a0351bc0311ffd69a
kingorianderson <kingorianderson@gmail.com>
2022-02-06 11:19:20 -0800

    commit feb 6

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,8 +7,26 @@ const Container = styled.div``;
 const Wrapper = styled.div`
   padding: 20px;
 `;
-const Title = styled.h1``;
-const Top = styled.div``;
+const Title = styled.h1`
+  font-weight: 300;
+  text-align: center;
+`;
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+const TopButton = styled.button`
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${(props) => props.type === "filled" && "none"};
+  background-color: ${(props) =>
+    props.type === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.type === "filled" && "white"};
+`;
 const Bottom = styled.div``;
 
 const Cart = () => {
@@ -18,7 +36,14 @@ const Cart = () => {
           <Announcement/>
           <Wrapper>
               <Title>YOUR BAG</Title>
-              <Top></Top>
+              <Top>
+                  <TopButton>CONTINUE SHOPPING</TopButton>
+                  <TopButton>CHECKOUT NOW</TopButton>
+                  <TopTexts>
+                      <TopText>Shopping Bag (2)</TopText>
+                      <TopText>Your Wishlist (0)</TopText>
+                  </TopTexts>
+              </Top>
               <Bottom></Bottom>
           </Wrapper>
 
@@ -27,4 +52,4 @@ const Cart = () => {
   )
 };
 
-ex
+export default Cart;

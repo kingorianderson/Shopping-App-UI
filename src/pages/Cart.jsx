a41b52ddd3de126403bee3b26ce938f9ded3ff5f
kingorianderson <kingorianderson@gmail.com>
2022-02-08 11:15:02 -0800

    commit

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -26,8 +26,8 @@ const TopButton = styled.button`
   cursor: pointer;
   border: ${(props) => props.type === "filled" && "none"};
   background-color: ${(props) =>
-                      props.type === "filled" ? "black" : "transparent"};
-  color: ${(props) => props.type === "filled" && "white"};
+                    props.type === "filled" ? "black" : "transparent"};
+  color:            ${(props) => props.type === "filled" && "white"};
 `;
 const TopTexts = styled.div``;
 const TopText = styled.span`
@@ -105,7 +105,27 @@ const Summary = styled.div`
   height: 50vh;
 `;
 
-
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`;
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${props=> props.type === "total" && "500"};
+  font-size: ${props=> props.type === "total" && "24px"};
+`;
+const SummaryItemText = styled.span`
+  
+`;
+const SummaryItemPrice = styled.span``;
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
+`;
 
 const Cart = () => {
   return (
@@ -116,11 +136,11 @@ const Cart = () => {
               <Title>YOUR BAG</Title>
               <Top>
                   <TopButton>CONTINUE SHOPPING</TopButton>
-                  <TopButton>CHECKOUT NOW</TopButton>
                   <TopTexts>
                       <TopText>Shopping Bag (2)</TopText>
                       <TopText>Your Wishlist (0)</TopText>
                   </TopTexts>
+                  <TopButton type="filled">CHECKOUT NOW</TopButton>
               </Top>
               <Bottom>
                 <Info>
@@ -176,7 +196,26 @@ const Cart = () => {
               </PriceDetail>
             </Product>
                 </Info>
-                <Summary>Summary</Summary>
+                <Summary>
+                  <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+                  <SummaryItem>
+                    <SummaryItemText>Subtotal</SummaryItemText>
+                    <SummaryItemPrice>Kes 5050</SummaryItemPrice>
+                  </SummaryItem>
+                  <SummaryItem>
+                    <SummaryItemText>Estimated Shipping</SummaryItemText>
+                    <SummaryItemPrice>Kes 200</SummaryItemPrice>
+                  </SummaryItem>
+                  <SummaryItem>
+                    <SummaryItemText>Shipping Discount</SummaryItemText>
+                    <SummaryItemPrice>Kes -200</SummaryItemPrice>
+                  </SummaryItem>
+                  <SummaryItem  type="total">
+                    <SummaryItemText>Total</SummaryItemText>
+                    <SummaryItemPrice>Kes 5050</SummaryItemPrice>
+                  </SummaryItem>
+                  <Button>CHECKOUT NOW</Button>
+                </Summary>
               </Bottom>
           </Wrapper>
 
